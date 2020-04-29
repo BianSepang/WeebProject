c9cff0b3649c46cc07d4d6105646682eea6c44a9
Adek Maulana <adek@techdro.id>
2020-04-29 20:32:21 +0700

    filter: update regex filter to use dot
    Change-Id: Ic1c145cafc09a4ddf978e74042e4077cc584e038

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -37,7 +37,7 @@ async def filter_incoming_handler(handler):
         pass
 
 
-@register(outgoing=True, pattern=r"^.filter ((@)?\w*)")
+@register(outgoing=True, pattern=r"^.filter (.*)")
 async def add_new_filter(new_handler):
     """ For .filter command, allows adding new filters in a chat """
     try:
@@ -75,7 +75,7 @@ async def add_new_filter(new_handler):
         await new_handler.edit(success.format(keyword, 'updated'))
 
 
-@register(outgoing=True, pattern=r"^.stop ((@)?\w*)")
+@register(outgoing=True, pattern=r"^.stop (.*)")
 async def remove_a_filter(r_handler):
     """ For .stop command, allows you to remove a filter from a chat. """
     try:
