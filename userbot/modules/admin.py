1f7696d25d84c3c699665984fdc44ce9714d8d6c
BianSepang <bianconneri46@gmail.com>
2020-11-04 19:28:43 +0800

    admin: update
    - add global mute
    - add tag all
    
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -33,7 +33,7 @@ from telethon.tl.types import (
     PeerChat,
 )
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
 from userbot.events import register
 
 # =================== CONSTANT ===================
@@ -411,14 +411,16 @@ async def unmoot(unmot):
             )
 
 
-@register(incoming=True)
+@register(incoming=True, disable_errors=True)
 async def muter(moot):
-    """ Used for deleting the messages of muted people """
+    """Used for deleting the messages of muted people"""
     try:
+        from userbot.modules.sql_helper.gmute_sql import is_gmuted
         from userbot.modules.sql_helper.spam_mute_sql import is_muted
     except AttributeError:
         return
     muted = is_muted(moot.chat_id)
+    gmuted = is_gmuted(moot.sender_id)
     rights = ChatBannedRights(
         until_date=None,
         send_messages=True,
@@ -432,10 +434,122 @@ async def muter(moot):
     if muted:
         for i in muted:
             if str(i.sender) == str(moot.sender_id):
-                await moot.delete()
-                await moot.client(
-                    EditBannedRequest(moot.chat_id, moot.sender_id, rights)
-                )
+                try:
+                    await moot.delete()
+                    await moot.client(
+                        EditBannedRequest(moot.chat_id, moot.sender_id, rights)
+                    )
+                except (
+                    BadRequestError,
+                    UserAdminInvalidError,
+                    ChatAdminRequiredError,
+                    UserIdInvalidError,
+                ):
+                    await moot.client.send_read_acknowledge(moot.chat_id, moot.id)
+    for i in gmuted:
+        if i.sender == str(moot.sender_id):
+            await moot.delete()
+
+
+@register(outgoing=True, pattern=r"^\.ungmute(?: |$)(.*)")
+async def ungmoot(un_gmute):
+    """For .ungmute command, ungmutes the target in the userbot"""
+    # Admin or creator check
+    chat = await un_gmute.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await un_gmute.edit(NO_ADMIN)
+        return
+
+    # Check if the function running under SQL mode
+    try:
+        from userbot.modules.sql_helper.gmute_sql import ungmute
+    except AttributeError:
+        await un_gmute.edit(NO_SQL)
+        return
+
+    user = await get_user_from_event(un_gmute)
+    user = user[0]
+    if not user:
+        return
+
+    # If pass, inform and start ungmuting
+    await un_gmute.edit("```Ungmuting...```")
+
+    if ungmute(user.id) is False:
+        await un_gmute.edit("`Error! User probably not gmuted.`")
+    else:
+        # Inform about success
+        await un_gmute.edit("```Ungmuted Successfully```")
+        await sleep(3)
+        await un_gmute.delete()
+
+        if BOTLOG:
+            await un_gmute.client.send_message(
+                BOTLOG_CHATID,
+                "#UNGMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {un_gmute.chat.title}(`{un_gmute.chat_id}`)",
+            )
+
+
+@register(outgoing=True, pattern=r"^\.gmute(?: |$)(.*)")
+async def gspider(gspdr):
+    """For .gmute command, globally mutes the replied/tagged person"""
+    # Admin or creator check
+    chat = await gspdr.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # If not admin and not creator, return
+    if not admin and not creator:
+        await gspdr.edit(NO_ADMIN)
+        return
+
+    # Check if the function running under SQL mode
+    try:
+        from userbot.modules.sql_helper.gmute_sql import gmute
+    except AttributeError:
+        await gspdr.edit(NO_SQL)
+        return
+
+    user, reason = await get_user_from_event(gspdr)
+    if not user:
+        return
+
+    # If pass, inform and start gmuting
+    await gspdr.edit("`Grabs a huge, sticky duct tape!`")
+    if gmute(user.id) is False:
+        await gspdr.edit("`Error! User probably already gmuted.\nRe-rolls the tape.`")
+    else:
+        if reason:
+            await gspdr.edit(f"`Globally taped!`\nReason: {reason}")
+        else:
+            await gspdr.edit("`Globally taped!`")
+
+        if BOTLOG:
+            await gspdr.client.send_message(
+                BOTLOG_CHATID,
+                "#GMUTE\n"
+                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                f"CHAT: {gspdr.chat.title}(`{gspdr.chat_id}`)",
+            )
+
+
+@register(outgoing=True, pattern=r"^\.all$")
+async def tagaso(event):
+    """For .all command, mention all of the member in the group chat"""
+    if event.fwd_from:
+        return
+    await event.delete()
+    mentions = "@all"
+    chat = await event.get_input_chat()
+    async for user in bot.iter_participants(chat, 500):
+        mentions += f"[\u2063](tg://user?id={user.id})"
+    await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
 @register(outgoing=True, pattern="^.zombies(?: |$)(.*)", groups_only=False)
