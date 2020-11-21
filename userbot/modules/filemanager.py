dd3fe480909a3a8396d25016df89c53f6bc1d434
BianSepang <bianconneri46@gmail.com>
2020-11-21 08:05:12 +0800

    filemanager: add remove and rename
    * and little bit of improvements
    
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/filemanager.py
+++ userbot/modules/filemanager.py
@@ -1,8 +1,12 @@
+# Credits to Userge for Remove and Rename
+
 import io
 import os
 import os.path
+import shutil
 import time
-from os.path import exists, isdir
+from os.path import dirname, exists, isdir, isfile, join
+from shutil import rmtree
 
 from userbot import CMD_HELP
 from userbot.events import register
@@ -16,10 +20,7 @@ async def lst(event):
     if event.fwd_from:
         return
     cat = event.pattern_match.group(1)
-    if cat:
-        path = cat
-    else:
-        path = os.getcwd()
+    path = cat if cat else os.getcwd()
     if not exists(path):
         await event.edit(
             f"There is no such directory or file with the name `{cat}` check again!"
@@ -27,11 +28,10 @@ async def lst(event):
         return
     if isdir(path):
         if cat:
-            msg = "Folders and Files in `{}` :\n".format(path)
-            lists = os.listdir(path)
+            msg = "**Folders and Files in `{}`** :\n\n".format(path)
         else:
-            msg = "Folders and Files in Current Directory :\n"
-            lists = os.listdir(path)
+            msg = "**Folders and Files in Current Directory** :\n\n"
+        lists = os.listdir(path)
         files = ""
         folders = ""
         for contents in sorted(lists):
@@ -39,47 +39,62 @@ async def lst(event):
             if not isdir(catpath):
                 size = os.stat(catpath).st_size
                 if contents.endswith((".mp3", ".flac", ".wav", ".m4a")):
-                    files += "🎵" + f"`{contents}`\n"
+                    files += "🎵 " + f"`{contents}`\n"
                 if contents.endswith((".opus")):
-                    files += "🎙" + f"`{contents}`\n"
+                    files += "🎙 " + f"`{contents}`\n"
                 elif contents.endswith(
                     (".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")
                 ):
-                    files += "🎞" + f"`{contents}`\n"
-                elif contents.endswith((".zip", ".tar", ".tar.gz", ".rar")):
-                    files += "🗜" + f"`{contents}`\n"
+                    files += "🎞 " + f"`{contents}`\n"
                 elif contents.endswith(
-                    (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico")
+                    (".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")
                 ):
-                    files += "🖼" + f"`{contents}`\n"
+                    files += "🗜 " + f"`{contents}`\n"
+                elif contents.endswith(
+                    (".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")
+                ):
+                    files += "🖼 " + f"`{contents}`\n"
+                elif contents.endswith((".exe", ".deb")):
+                    files += "⚙️ " + f"`{contents}`\n"
+                elif contents.endswith((".iso", ".img")):
+                    files += "💿 " + f"`{contents}`\n"
+                elif contents.endswith((".apk", ".xapk")):
+                    files += "📱 " + f"`{contents}`\n"
+                elif contents.endswith((".py")):
+                    files += "🐍 " + f"`{contents}`\n"
                 else:
-                    files += "📄" + f"`{contents}`\n"
+                    files += "📄 " + f"`{contents}`\n"
             else:
-                folders += f"📁`{contents}`\n"
-        if files or folders:
-            msg = msg + folders + files
-        else:
-            msg = msg + "__empty path__"
+                folders += f"📁 `{contents}`\n"
+        msg = msg + folders + files if files or folders else msg + "__empty path__"
     else:
         size = os.stat(path).st_size
-        msg = f"The details of given file :\n"
+        msg = "The details of given file :\n\n"
         if path.endswith((".mp3", ".flac", ".wav", ".m4a")):
-            mode = "🎵"
+            mode = "🎵 "
         if path.endswith((".opus")):
-            mode = "🎙"
+            mode = "🎙 "
         elif path.endswith((".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")):
-            mode = "🎞"
-        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar")):
-            mode = "🗜"
-        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico")):
-            mode = "🖼"
+            mode = "🎞 "
+        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")):
+            mode = "🗜 "
+        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")):
+            mode = "🖼 "
+        elif path.endswith((".exe", ".deb")):
+            mode = "⚙️ "
+        elif path.endswith((".iso", ".img")):
+            mode = "💿 "
+        elif path.endswith((".apk", ".xapk")):
+            mode = "📱 "
+        elif path.endswith((".py")):
+            mode = "🐍 "
         else:
-            mode = "📄"
+            mode = "📄 "
         time.ctime(os.path.getctime(path))
         time2 = time.ctime(os.path.getmtime(path))
         time3 = time.ctime(os.path.getatime(path))
         msg += f"**Location :** `{path}`\n"
-        msg += f"**icon :** `{mode}`\n"
+        msg += f"**Icon :** `{mode}`\n"
         msg += f"**Size :** `{humanbytes(size)}`\n"
         msg += f"**Last Modified Time:** `{time2}`\n"
         msg += f"**Last Accessed Time:** `{time3}`"
@@ -99,6 +114,43 @@ async def lst(event):
         await event.edit(msg)
 
 
+@register(outgoing=True, pattern=r"^\.rm ?(.*)")
+async def rmove(event):
+    """Removing Directory/File"""
+    cat = event.pattern_match.group(1)
+    if not cat:
+        await event.edit("`Missing file path!`")
+        return
+    if not exists(cat):
+        await event.edit("`File path not exists!`")
+        return
+    if isfile(cat):
+        os.remove(cat)
+    else:
+        rmtree(cat)
+    await event.edit(f"Removed `{cat}`")
+
+
+@register(outgoing=True, pattern=r"^\.rn ([^|]+)\|([^|]+)")
+async def rname(event):
+    """Renaming Directory/File"""
+    cat = str(event.pattern_match.group(1)).strip()
+    new_name = str(event.pattern_match.group(2)).strip()
+    if not exists(cat):
+        await event.edit(f"file path : {cat} not exists!")
+        return
+    new_path = join(dirname(cat), new_name)
+    shutil.move(cat, new_path)
+    await event.edit(f"Renamed `{cat}` to `{new_path}`")
+
+
 CMD_HELP.update(
-    {"file": ">`.ls` <directory>" "\nUsage: Get list file inside directory.\n"}
+    {
+        "file": ">`.ls` <directory>"
+        "\nUsage: Get list file inside directory."
+        "\n\n>`.rm` <directory/file>"
+        "\nUsage: remove file or directory"
+        "\n\n>`.rn` <directory/file> | <new name>"
+        "\nUsage: rename file or directory"
+    }
 )
