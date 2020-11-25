809a28d2427e268a8305d8bc3d365edd2219ca93
BianSepang <bianconneri46@gmail.com>
2020-11-25 19:20:17 +0800

    nhentai: handle TimeoutError
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/nhentai.py
+++ userbot/modules/nhentai.py
@@ -4,6 +4,8 @@
 # you may not use this file except in compliance with the License.
 #
 
+from asyncio.exceptions import TimeoutError
+
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
@@ -17,22 +19,25 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     chat = "@nHentaiBot"
-    await event.edit("```Processing```")
-    async with bot.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=424466890)
-            )
-            await bot.send_message(chat, link)
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("```Please unblock @nHentaiBot and try again```")
-            return
-        if response.text.startswith("**Sorry I couldn't get manga from**"):
-            await event.edit("```I think this is not the right link```")
-        else:
-            await event.delete()
-            await bot.send_message(event.chat_id, response.message)
+    try:
+        await event.edit("```Processing```")
+        async with bot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=424466890)
+                )
+                await bot.send_message(chat, link)
+                response = await response
+            except YouBlockedUserError:
+                await event.reply("```Please unblock @nHentaiBot and try again```")
+                return
+            if response.text.startswith("**Sorry I couldn't get manga from**"):
+                await event.edit("```I think this is not the right link```")
+            else:
+                await event.delete()
+                await bot.send_message(event.chat_id, response.message)
+    except TimeoutError:
+        await event.edit("`Error: ``@nHentaiBot`` is not responding!`")
 
 
 CMD_HELP.update(
