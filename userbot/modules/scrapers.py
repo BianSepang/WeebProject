6c3bfb02f938b4b1090088be3283dc973aeacd1f
KenHV <yo@kenharris.xyz>
2021-03-24 10:38:59 +0530

    scrapers: ytdl: switch to yt-dlp
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -27,8 +27,9 @@ from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 from urbandict import define
 from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
-from youtube_dl import YoutubeDL
-from youtube_dl.utils import (
+from youtube_search import YoutubeSearch
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import (
     ContentTooShortError,
     DownloadError,
     ExtractorError,
@@ -38,7 +39,6 @@ from youtube_dl.utils import (
     UnavailableVideoError,
     XAttrMetadataError,
 )
-from youtube_search import YoutubeSearch
 
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
@@ -581,6 +581,7 @@ async def download_video(v_url):
             "outtmpl": "%(id)s.mp3",
             "quiet": True,
             "logtostderr": False,
+            "external_downloader": "aria2c",
         }
         video = False
         song = True
@@ -599,6 +600,7 @@ async def download_video(v_url):
             "outtmpl": "%(id)s.mp4",
             "logtostderr": False,
             "quiet": True,
+            "external_downloader": "aria2c",
         }
         song = False
         video = True
