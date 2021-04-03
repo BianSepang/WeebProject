87bbb53bdbcd7dc877fb672375c5b8c8c6fb57b0
Bian <bianconneri46@gmail.com>
2021-04-03 18:13:05 +0800

    scrapers: google: catch some exceptions
    Signed-off-by: Bian <bianconneri46@gmail.com>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -173,18 +173,21 @@ async def gsearch(q_event):
         match = match.replace("page=" + page[0], "")
     except IndexError:
         page = 1
-    search_args = (str(match), int(page))
-    gsearch = GoogleSearch()
-    gresults = await gsearch.async_search(*search_args)
-    msg = ""
-    for i in range(5):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            msg += f"[{title}]({link})\n`{desc}`\n\n"
-        except IndexError:
-            break
+    try:
+        search_args = (str(match), int(page))
+        gsearch = GoogleSearch()
+        gresults = await gsearch.async_search(*search_args)
+        msg = ""
+        for i in range(5):
+            try:
+                title = gresults["titles"][i]
+                link = gresults["links"][i]
+                desc = gresults["descriptions"][i]
+                msg += f"[{title}]({link})\n`{desc}`\n\n"
+            except IndexError:
+                break
+    except BaseException as g_e:
+        return await event.edit(f"**Error : ** `{g_e}`")
     await q_event.edit(
         "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
     )
