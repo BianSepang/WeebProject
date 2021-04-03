e6171667aa3af35c560874dccf7c665c64544379
enzooys <79890153+enzooys@users.noreply.github.com>
2021-04-03 14:29:51 +0700

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -28,7 +28,6 @@ from urbandict import define
 from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
 from youtube_search import YoutubeSearch
-from yt_dlp import YoutubeDL
 from yt_dlp.utils import (
     ContentTooShortError,
     DownloadError,
