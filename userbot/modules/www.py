6eb34f9704c2c682dc081e3d1f94a2efb802498f
Bian <bianconneri46@gmail.com>
2021-04-08 19:49:13 +0800

    www: speedtest: improve output
    Signed-off-by: Bian <bianconneri46@gmail.com>

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -13,47 +13,42 @@ from telethon import functions
 
 from userbot import CMD_HELP
 from userbot.events import register
+from userbot.utils import humanbytes
 
 
-@register(outgoing=True, pattern=r"^\.speed$")
+@register(outgoing=True, pattern=r"^\.speedtest$")
 async def speedtst(spd):
     """ For .speed command, use SpeedTest to check server speeds. """
-    await spd.edit("`Running speed test . . .`")
-    test = Speedtest()
+    await spd.edit("`Running speed test...`")
 
+    test = Speedtest()
     test.get_best_server()
     test.download()
     test.upload()
     test.results.share()
     result = test.results.dict()
 
-    await spd.edit(
-        "`"
-        "Started at "
-        f"{result['timestamp']} \n\n"
-        "Download "
-        f"{speed_convert(result['download'])} \n"
-        "Upload "
-        f"{speed_convert(result['upload'])} \n"
-        "Ping "
-        f"{result['ping']} \n"
-        "ISP "
-        f"{result['client']['isp']}"
-        "`"
+    msg = (
+        f"**Started at {result['timestamp']}**\n\n"
+        "**Client**\n"
+        f"**ISP :** `{result['client']['isp']}`\n"
+        f"**Country :** `{result['client']['country']}`\n\n"
+        "**Server**\n"
+        f"**Name :** `{result['server']['name']}`\n"
+        f"**Country :** `{result['server']['country']}`\n"
+        f"**Sponsor :** `{result['server']['sponsor']}`\n\n"
+        f"**Ping :** `{result['ping']}`\n"
+        f"**Upload :** `{humanbytes(result['upload'])}/s`\n"
+        f"**Download :** `{humanbytes(result['download'])}/s`"
     )
 
-
-def speed_convert(size):
-    """
-    Hi human, you can't read bytes?
-    """
-    power = 2 ** 10
-    zero = 0
-    units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
-    while size > power:
-        size /= power
-        zero += 1
-    return f"{round(size, 2)} {units[zero]}"
+    await spd.delete()
+    await spd.client.send_file(
+        spd.chat_id,
+        result["share"],
+        caption=msg,
+        force_document=False,
+    )
 
 
 @register(outgoing=True, pattern=r"^\.dc$")
@@ -79,7 +74,7 @@ async def pingme(pong):
 
 CMD_HELP.update(
     {
-        "speed": ">`.speed`" "\nUsage: Does a speedtest and shows the results.",
+        "speedtest": ">`.speedtest`" "\nUsage: Does a speedtest and shows the results.",
         "dc": ">`.dc`" "\nUsage: Finds the nearest datacenter from your server.",
         "ping": ">`.ping`" "\nUsage: Shows how long it takes to ping your bot.",
     }
