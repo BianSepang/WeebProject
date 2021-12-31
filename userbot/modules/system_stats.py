0f6eaadb4b10a17b76482020282a04ebeefa95f8
Bian <bianconneri46@gmail.com>
2022-01-01 03:42:22 +0800

    system_stats: cleanup
    * remove `.pip` command
    * de-emojify `.alive` command
    
    Signed-off-by: Bian <bianconneri46@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -7,7 +7,6 @@
 
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
-from os import remove
 from platform import python_version, uname
 from shutil import which
 
@@ -15,7 +14,7 @@ from git import Repo
 from telethon import version
 from telethon.errors.rpcerrorlist import MediaEmptyError
 
-from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP, bot
+from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP
 from userbot.events import register
 
 # ================= CONSTANT =================
@@ -80,70 +79,18 @@ async def bot_ver(event):
             )
 
 
-@register(outgoing=True, pattern=r"^\.pip(?: |$)(.*)")
-async def pipcheck(pip):
-    """For .pip command, do a pip search."""
-    if not pip.text[0].isalpha() and pip.text[0] not in ("/", "#", "@", "!"):
-        pipmodule = pip.pattern_match.group(1)
-        if pipmodule:
-            await pip.edit("`Searching . . .`")
-            pipc = await asyncrunapp(
-                "pip3",
-                "search",
-                pipmodule,
-                stdout=asyncPIPE,
-                stderr=asyncPIPE,
-            )
-
-            stdout, stderr = await pipc.communicate()
-            pipout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-            if pipout:
-                if len(pipout) > 4096:
-                    await pip.edit("`Output too large, sending as file`")
-                    file = open("output.txt", "w+")
-                    file.write(pipout)
-                    file.close()
-                    await pip.client.send_file(
-                        pip.chat_id,
-                        "output.txt",
-                        reply_to=pip.id,
-                    )
-                    remove("output.txt")
-                    return
-                await pip.edit(
-                    "**Query: **\n`"
-                    f"pip3 search {pipmodule}"
-                    "`\n**Result: **\n`"
-                    f"{pipout}"
-                    "`"
-                )
-            else:
-                await pip.edit(
-                    "**Query: **\n`"
-                    f"pip3 search {pipmodule}"
-                    "`\n**Result: **\n`No Result Returned/False`"
-                )
-        else:
-            await pip.edit("`Use .help pip to see an example`")
-
-
 @register(outgoing=True, pattern=r"^\.(alive|on)$")
 async def amireallyalive(alive):
     """For .alive command, check if the bot is running."""
-    logo = ALIVE_LOGO
     output = (
-        f"`WeebProject` is running on `{repo.active_branch.name}`\n"
-        "`====================================`\n"
-        f"🐍 `Python         :` v{python_version()}\n"
-        f"⚙️ `Telethon       :` v{version.__version__}\n"
-        f"👤 `User           :` {DEFAULTUSER}\n"
-        "`====================================`\n"
+        f"**WeebProject** is running on **{repo.active_branch.name}**\n\n"
+        f"**Python :** __v{python_version()}__\n"
+        f"**Telethon :** __v{version.__version__}__\n"
+        f"**User :** __{DEFAULTUSER}__"
     )
     if ALIVE_LOGO:
         try:
-            logo = ALIVE_LOGO
-            await bot.send_file(alive.chat_id, logo, caption=output)
+            await alive.respond(output, file=ALIVE_LOGO)
             await alive.delete()
         except MediaEmptyError:
             await alive.edit(
@@ -154,17 +101,13 @@ async def amireallyalive(alive):
         await alive.edit(output)
 
 
-@register(outgoing=True, pattern=r"^\.aliveu")
+@register(outgoing=True, pattern=r"^\.aliveu (.+)")
 async def amireallyaliveuser(username):
     """For .aliveu command, change the username in the .alive command."""
-    message = username.text
-    output = ".aliveu [new user without brackets] nor can it be empty"
-    if not (message == ".aliveu" or message[7:8] != " "):
-        newuser = message[8:]
-        global DEFAULTUSER
-        DEFAULTUSER = newuser
-        output = "Successfully changed user to " + newuser + "!"
-    await username.edit("`" f"{output}" "`")
+    newuser = username.pattern_match.group(1)
+    global DEFAULTUSER
+    DEFAULTUSER = newuser
+    await username.edit(f"Successfully changed user to `{newuser}`")
 
 
 @register(outgoing=True, pattern=r"^\.resetalive$")
@@ -179,7 +122,6 @@ CMD_HELP.update(
     {
         "sysd": ">`.sysd`" "\nUsage: Shows system information using neofetch.",
         "botver": ">`.botver`" "\nUsage: Shows the userbot version.",
-        "pip": ">`.pip <module(s)>`" "\nUsage: Does a search of pip modules(s).",
         "alive": ">`.alive`"
         "\nUsage: Type .alive to see wether your bot is working or not."
         "\n\n>`.aliveu <text>`"
