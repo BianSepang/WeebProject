ff7529f57db996103889177bbb6caaa2aac99451
Bian <bianconneri46@gmail.com>
2022-07-12 23:12:14 +0800

    modules: remove `.vsong`, `.song`, `.pixeldl`, `.nhentai`
    Signed-off-by: Bian <bianconneri46@gmail.com>

--- userbot/modules/android.py
+++ userbot/modules/android.py
@@ -5,19 +5,14 @@
 #
 """ Userbot module containing commands related to android"""
 
-import asyncio
-import math
-import os
 import re
-import time
 
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from requests import get
 
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
+from userbot import CMD_HELP
 from userbot.events import register
-from userbot.utils import chrome, human_to_bytes, humanbytes, md5, time_formatter
 
 GITHUB = "https://github.com"
 DEVICES_DATA = (
@@ -115,113 +110,6 @@ async def codename_info(request):
     else:
         reply = f"`Couldn't find {device} codename!`\n"
     await request.edit(reply)
-
-
-@register(outgoing=True, pattern=r"^\.pixeldl(?: |$)(.*)")
-async def download_api(dl):
-    await dl.edit("`Collecting information...`")
-    URL = dl.pattern_match.group(1)
-    URL_MSG = await dl.get_reply_message()
-    if URL:
-        pass
-    elif URL_MSG:
-        URL = URL_MSG.text
-    else:
-        await dl.edit("`Empty information...`")
-        return
-    if not re.findall(r"\bhttps?://download.*pixelexperience.*\.org\S+", URL):
-        await dl.edit("`Invalid information...`")
-        return
-    driver = await chrome()
-    await dl.edit("`Getting information...`")
-    driver.get(URL)
-    error = driver.find_elements_by_class_name("swal2-content")
-    if len(error) > 0:
-        if error[0].text == "File Not Found.":
-            await dl.edit(f"`FileNotFoundError`: {URL} is not found.")
-            return
-    datas = driver.find_elements_by_class_name("download__meta")
-    """ - enumerate data to make sure we download the matched version - """
-    md5_origin = None
-    i = None
-    for index, value in enumerate(datas):
-        for data in value.text.split("\n"):
-            if data.startswith("MD5"):
-                md5_origin = data.split(":")[1].strip()
-                i = index
-                break
-        if md5_origin is not None and i is not None:
-            break
-    if md5_origin is None and i is None:
-        await dl.edit("`There is no match version available...`")
-    if URL.endswith("/"):
-        file_name = URL.split("/")[-2]
-    else:
-        file_name = URL.split("/")[-1]
-    file_path = TEMP_DOWNLOAD_DIRECTORY + file_name
-    download = driver.find_elements_by_class_name("download__btn")[i]
-    download.click()
-    await dl.edit("`Starting download...`")
-    file_size = human_to_bytes(download.text.split(None, 2)[-1].strip("()"))
-    display_message = None
-    complete = False
-    start = time.time()
-    while complete is False:
-        if os.path.isfile(file_path + ".crdownload"):
-            try:
-                downloaded = os.stat(file_path + ".crdownload").st_size
-                status = "Downloading"
-            except OSError:  # Rare case
-                await asyncio.sleep(1)
-                continue
-        elif os.path.isfile(file_path):
-            downloaded = os.stat(file_path).st_size
-            file_size = downloaded
-            status = "Checking"
-        else:
-            await asyncio.sleep(0.3)
-            continue
-        diff = time.time() - start
-        percentage = downloaded / file_size * 100
-        speed = round(downloaded / diff, 2)
-        eta = round((file_size - downloaded) / speed)
-        prog_str = "`{}` | [{}{}] `{}%`".format(
-            status,
-            "".join(["●" for i in range(math.floor(percentage / 10))]),
-            "".join(["○" for i in range(10 - math.floor(percentage / 10))]),
-            round(percentage, 2),
-        )
-        current_message = (
-            "`[DOWNLOAD]`\n\n"
-            f"`{file_name}`\n"
-            f"`Status`\n{prog_str}\n"
-            f"`{humanbytes(downloaded)} of {humanbytes(file_size)}"
-            f" @ {humanbytes(speed)}`\n"
-            f"`ETA` -> {time_formatter(eta)}"
-        )
-        if (
-            round(diff % 15.00) == 0
-            and display_message != current_message
-            or (downloaded == file_size)
-        ):
-            await dl.edit(current_message)
-            display_message = current_message
-        if downloaded == file_size:
-            if not os.path.isfile(file_path):  # Rare case
-                await asyncio.sleep(1)
-                continue
-            MD5 = await md5(file_path)
-            if md5_origin == MD5:
-                complete = True
-            else:
-                await dl.edit("`Download corrupt...`")
-                os.remove(file_path)
-                driver.quit()
-                return
-    await dl.respond(f"`{file_name}`\n\n" f"Successfully downloaded to `{file_path}`.")
-    await dl.delete()
-    driver.quit()
-    return
 
 
 @register(outgoing=True, pattern=r"^\.specs(?: |)([\S]*)(?: |)([\s\S]*)")
--- userbot/modules/getmusic.py
+++ userbot/modules/getmusic.py
@@ -2,19 +2,15 @@
 # All rights reserved.
 
 import asyncio
-import glob
 import os
 import shutil
-import subprocess
 import time
 
 import deezloader
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
 from pylast import User
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
+from telethon.tl.types import DocumentAttributeAudio
 
 from userbot import (
     CMD_HELP,
@@ -27,153 +23,8 @@ from userbot import (
     lastfm,
 )
 from userbot.events import register
-from userbot.utils import chrome, progress
+from userbot.utils import progress
 from userbot.utils.FastTelethon import upload_file
-
-
-async def getmusic(cat):
-    video_link = ""
-    search = cat
-    driver = await chrome()
-    driver.get("https://www.youtube.com/results?search_query=" + search)
-    user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
-    for i in user_data:
-        video_link = i.get_attribute("href")
-        break
-    command = (
-        f"yt-dlp -x --add-metadata --embed-thumbnail --audio-format mp3 {video_link}"
-    )
-    os.system(command)
-    return video_link
-
-
-async def getmusicvideo(cat):
-    video_link = ""
-    search = cat
-    driver = await chrome()
-    driver.get("https://www.youtube.com/results?search_query=" + search)
-    user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
-    for i in user_data:
-        video_link = i.get_attribute("href")
-        break
-    command = 'yt-dlp -f "[filesize<50M]" --merge-output-format mp4 ' + video_link
-    os.system(command)
-
-
-@register(outgoing=True, pattern=r"^\.song (.*)")
-async def _(event):
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-        await event.edit("`Wait..! I am finding your song..`")
-    elif reply.message:
-        query = reply.message
-        await event.edit("`Wait..! I am finding your song..`")
-    else:
-        await event.edit("`What I am Supposed to find?`")
-        return
-
-    video_link = await getmusic(str(query))
-    loa = glob.glob("*.mp3")[0]
-    await event.edit("`Yeah.. Uploading your song..`")
-    c_time = time.time()
-    with open(loa, "rb") as f:
-        result = await upload_file(
-            client=event.client,
-            file=f,
-            name=loa,
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(d, t, event, c_time, "[UPLOAD]", loa)
-            ),
-        )
-    await event.client.send_file(
-        event.chat_id,
-        result,
-        allow_cache=False,
-        caption=f"[{query}]({video_link})",
-        reply_to=reply_to_id,
-    )
-    await event.delete()
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3", shell=True)
-
-
-@register(outgoing=True, pattern=r"^\.vsong(?: |$)(.*)")
-async def _(event):
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-        await event.edit("`Wait..! I am finding your videosong..`")
-    elif reply:
-        query = str(reply.message)
-        await event.edit("`Wait..! I am finding your videosong..`")
-    else:
-        await event.edit("`What I am Supposed to find?`")
-        return
-    await getmusicvideo(query)
-    l = glob.glob("*.mp4") + glob.glob("*.mkv") + glob.glob("*.webm")
-    if l:
-        await event.edit("`Yeah..! i found something..`")
-    else:
-        await event.edit(f"`Sorry..! i can't find anything with` **{query}**")
-        return
-    try:
-        loa = l[0]
-        metadata = extractMetadata(createParser(loa))
-        duration = 0
-        width = 0
-        height = 0
-        if metadata.has("duration"):
-            duration = metadata.get("duration").seconds
-        if metadata.has("width"):
-            width = metadata.get("width")
-        if metadata.has("height"):
-            height = metadata.get("height")
-        os.system("cp *mp4 thumb.mp4")
-        os.system("ffmpeg -i thumb.mp4 -vframes 1 -an -s 480x360 -ss 5 thumb.jpg")
-        thumb_image = "thumb.jpg"
-        c_time = time.time()
-        with open(loa, "rb") as f:
-            result = await upload_file(
-                client=event.client,
-                file=f,
-                name=loa,
-                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                    progress(d, t, event, c_time, "[UPLOAD]", loa)
-                ),
-            )
-        await event.client.send_file(
-            event.chat_id,
-            result,
-            force_document=False,
-            thumb=thumb_image,
-            allow_cache=False,
-            caption=query,
-            supports_streaming=True,
-            reply_to=reply_to_id,
-            attributes=[
-                DocumentAttributeVideo(
-                    duration=duration,
-                    w=width,
-                    h=height,
-                    round_message=False,
-                    supports_streaming=True,
-                )
-            ],
-        )
-        await event.edit(f"**{query}** `Uploaded Successfully..!`")
-        os.remove(thumb_image)
-        os.system("rm *.mkv *.mp4 *.webm")
-    except BaseException:
-        os.remove(thumb_image)
-        os.system("rm *.mkv *.mp4 *.webm")
-        return
 
 
 @register(outgoing=True, pattern=r"^\.smd (?:(now)|(.*) - (.*))")
