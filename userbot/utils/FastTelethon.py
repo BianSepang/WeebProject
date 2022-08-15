93750b0d83d0b5534595a6d6d0a041c0a13c4ed5
Bian <bianconneri46@gmail.com>
2022-08-15 23:49:54 +0800

    pre-commit: update and run hooks
    Signed-off-by: Bian <bianconneri46@gmail.com>

--- userbot/utils/FastTelethon.py
+++ userbot/utils/FastTelethon.py
@@ -7,8 +7,8 @@ import logging
 import math
 import os
 from collections import defaultdict
-from typing import BinaryIO, DefaultDict, Optional, Union
 from collections.abc import AsyncGenerator, Awaitable
+from typing import BinaryIO, DefaultDict, Optional, Union
 
 from telethon import TelegramClient, helpers, utils
 from telethon.crypto import AuthKey
