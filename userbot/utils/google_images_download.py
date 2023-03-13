c8ad7237b67c219c4d38ee132bfed90c9570a1c5
Bian <bianconneri46@gmail.com>
2023-03-13 19:20:52 +0800

    google_images_download: fix NoneType error
    Signed-off-by: Bian <bianconneri46@gmail.com>

--- userbot/utils/google_images_download.py
+++ userbot/utils/google_images_download.py
@@ -675,7 +675,7 @@ class googleimagesdownload:
         main = data[3]
         info = data[9]
         if info is None:
-            info = data[11]
+            info = data[23]
         formatted_object = {}
         try:
             formatted_object["image_height"] = main[2]
